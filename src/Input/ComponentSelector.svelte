3cbc51d101d85f5f6726352818e5c66a0f885c00
Tan Li Hau <lhtan93@gmail.com>
2019-10-13 23:50:23 +0800

    remove scrolling when create new tab

--- src/Input/ComponentSelector.svelte
+++ src/Input/ComponentSelector.svelte
@@ -70,11 +70,6 @@
 
 		editing = component;
 
-		setTimeout(() => {
-			// TODO we can do this without IDs
-			document.getElementById(component.name).scrollIntoView(false);
-		});
-
 		components.update(components => components.concat(component));
 		handle_select(component);
 	}
